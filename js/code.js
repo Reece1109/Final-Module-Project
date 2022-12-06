a0f9a6c3ba191c624c0f851ef1e78339bcd43448
Reece1109 <reecesimon84@gmail.com>
2022-12-06 16:31:34 +0200

    Second Update

---
+++ js/code.js
@@ -0,0 +1,34 @@
+let products = JSON.parse(localStorage.getItem('products ')) ?
+JSON.parse(localStorage.getItem('products ')) : [
+    {
+        id: 1,
+        productName: "Portrait",
+        ImageUrl: "https://i.postimg.cc/zfY18t9Z/31193.jpg",
+        Price: "",
+        Description:"",
+    }
+
+    {
+        id: 1,
+        productName: "Portrait",
+        ImageUrl: "https://i.postimg.cc/zfY18t9Z/31193.jpg",
+        Price: "",
+        Description:"",
+    }
+
+    {
+        id: 1,
+        productName: "Portrait",
+        ImageUrl: "https://i.postimg.cc/zfY18t9Z/31193.jpg",
+        Price: "",
+        Description:"",
+    }
+
+    {
+        id: 1,
+        productName: "Portrait",
+        ImageUrl: "https://i.postimg.cc/zfY18t9Z/31193.jpg",
+        Price: "",
+        Description:"",
+    }
+];
